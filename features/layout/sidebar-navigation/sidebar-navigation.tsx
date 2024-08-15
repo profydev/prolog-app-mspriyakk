34a7a1cb69422eff32cf84652c3cdd10a61565a7
mspriyakk <mspriyakk@gmail.com>
2024-08-15 02:11:20 -0400

    Update to  show the large logo on mobile devices

--- features/layout/sidebar-navigation/sidebar-navigation.tsx
+++ features/layout/sidebar-navigation/sidebar-navigation.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Routes } from "@config/routes";
 import classNames from "classnames";
 import { NavigationContext } from "./navigation-context";
@@ -20,9 +20,24 @@ export function SidebarNavigation() {
   const router = useRouter();
   const { isSidebarCollapsed, toggleSidebar } = useContext(NavigationContext);
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const [isLargeView, setIsLargeView] = useState(true);
+
+  const handleResize = () => {
+    setIsLargeView(window.innerWidth >= 1024);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   const handleSupportClick = () => {
     window.open("mailto:support@prolog-app.com?subject=Support Request");
   };
+
   return (
     <div
       className={classNames(
@@ -40,7 +55,7 @@ export function SidebarNavigation() {
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
             src={
-              isSidebarCollapsed
+              isSidebarCollapsed && isLargeView
                 ? "/icons/logo-small.svg"
                 : "/icons/logo-large.svg"
             }
